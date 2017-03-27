ad6d4347ad04ba7fab1723fa7fea3d1dc53d9fac
InnaIgorevna <innaigorevnad@gmail.com>
2017-03-27 22:44:05 +0700

    Внесла правки по камментарии наставника

--- js/stat.js
+++ js/stat.js
@@ -1,35 +1,62 @@
-window.renderStatistics = function(ctx, names, times){
-	ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-	ctx.fillRect(110, 20, 420, 270);
-	ctx.fillStyle = 'white';
-	ctx.fillRect(100, 10, 420, 270);
-
-	ctx.font = '16px PT Mono';
-	ctx.fillStyle  = "black";
-	ctx.fillText('Ура, вы победили!', 130, 50);
-	ctx.fillText('Список результатов:', 130, 70);
-
-	var maxTime = times[0];
-	for (i = 1; i < times.length; i++) {
-		if(maxTime < times[i]){
-			maxTime = times[i];
+function getMaxResult(arr){
+	var max = arr[0];
+	for (i = 1; i < arr.length; i++) {
+		if(max < arr[i]){
+			max = arr[i];
 		}	
 	}
+	return max;
+}
 
-	for(i = 0; i < names.length; i++){
+function getColumnColor(name){
+	var transparency = Math.random();
+	var columnColor;
 
-		if (names[i] == 'Вы') {
-			ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-		} else {
-			ctx.fillStyle = 'rgba(0, 0, 255,' + Math.random() + ')';
-		}
-
-		var rectHeight = 150 * (times[i]/ maxTime);
-
-		ctx.fillRect(130 + (i * 90), 250 - rectHeight, 40, rectHeight);
-		ctx.fillStyle  = "black";
-		ctx.fillText(names[i], 130 + (i * 90), 270);
-		ctx.fillText(times[i].toFixed(0), 130+ (i * 90), 245 - rectHeight);
+	if (name == 'Вы') {
+		columnColor = 'rgba(255, 0, 0, 1)';
+	} else {
+		columnColor = 'rgba(0, 0, 255,' + transparency + ')';
 	}
+	return columnColor;
+}
 
+function drawHistogram(ctx, names, times, drawStartX, drawStartY){
+	var maxTime = getMaxResult(times);
+	var maxColumnHeight = 150;
+	var columnWidth = 40;
+	var columnOffset = 50;
+   
+	for(i = 0; i < names.length; i++){
+		var rectHeight = maxColumnHeight * (times[i]/ maxTime);
+		var columnStartX = drawStartX + (i * (columnWidth + columnOffset));
+		var columnStartY = maxColumnHeight + drawStartY - rectHeight;
+		
+		ctx.fillStyle = getColumnColor(names[i]);
+		ctx.fillRect(columnStartX, columnStartY, columnWidth, rectHeight);
+		ctx.fillStyle  = "black";
+		ctx.fillText(names[i], columnStartX, maxColumnHeight + drawStartY + 20);
+		ctx.fillText(times[i].toFixed(0), columnStartX, columnStartY - 5);
+	}
+}
+
+window.renderStatistics = function(ctx, names, times){
+   	var statisticsWindowX = 100;
+	var statisticsWindowY = 10;
+	var statisticsWindowWidth = 420;
+	var statisticsWindowHeight = 270;
+	var statisticsWindowFont = '16px PT Mono';
+	var drawStartX = statisticsWindowX + 30;
+	var drawStartY = 100;
+
+	ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+	ctx.fillRect(statisticsWindowX+10, statisticsWindowY+10, statisticsWindowWidth, statisticsWindowHeight);
+	ctx.fillStyle = 'white';
+	ctx.fillRect(statisticsWindowX , statisticsWindowY, statisticsWindowWidth, statisticsWindowHeight);
+
+	ctx.font = statisticsWindowFont;
+	ctx.fillStyle  = "black";
+	ctx.fillText('Ура, вы победили!', drawStartX, 50);
+	ctx.fillText('Список результатов:', drawStartX, 70);
+
+    drawHistogram(ctx, names, times, drawStartX, drawStartY);
 }
