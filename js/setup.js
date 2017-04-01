dab0c66bda51d249bfe25d4a2c956d190616711b
InnaIgorevna <innaigorevnad@gmail.com>
2017-04-01 21:43:30 +0700

    Правки ESLint

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const NAMES = [
+var NAMES = [
   'Иван',
   'Хуан Себастьян',
   'Мария',
@@ -11,7 +11,7 @@ const NAMES = [
   'Вашингтон'
 ];
 
-const SURNAMES = [
+var SURNAMES = [
   'да Марья',
   'Верон',
   'Мирабелла',
@@ -22,7 +22,7 @@ const SURNAMES = [
   'Ирвинг'
 ];
 
-const COAT_COLORS = [
+var COAT_COLORS = [
   'rgb(101, 137, 164)',
   'rgb(241, 43, 107)',
   'rgb(146, 100, 161)',
@@ -31,44 +31,44 @@ const COAT_COLORS = [
   'rgb(0, 0, 0)'
 ];
 
-const EYES_COLORS =[
+var EYES_COLORS = [
   'black',
   'red',
   'blue',
   'yellow',
   'green'
 ];
-//Возвращает рандомное целое число oт min до max
-function getRandom (min, max) {
+// Возвращает рандомное целое число oт min до max
+function getRandom(min, max) {
   var rand = min - 0.5 + Math.random() * (max - min + 1);
   rand = Math.round(rand);
   return rand;
 }
-//Возвращает рандомный элемет массива
-function getRandomArrayElement (arr) {
-  return arr[getRandom(0, arr.length-1)];
+// Возвращает рандомный элемет массива
+function getRandomArrayElement(arr) {
+  return arr[getRandom(0, arr.length - 1)];
 }
-//Возвращает полное имя волшебника
-function getWizardName () {
-  var rand = getRandom(0,1);
+// Возвращает полное имя волшебника
+function getWizardName() {
+  var rand = getRandom(0, 1);
   var name = getRandomArrayElement(NAMES);
   var surname = getRandomArrayElement(SURNAMES);
-  if(rand===0){
+  if (rand === 0) {
     return name + ' ' + surname;
-  }else{
+  } else {
     return surname + ' ' + name;
   }
 }
-//Возвращает цвет плаща
-function getWizardCoatColor () {
-  return  getRandomArrayElement(COAT_COLORS);
+// Возвращает цвет плаща
+function getWizardCoatColor() {
+  return getRandomArrayElement(COAT_COLORS);
 }
-//Возвращает цвет глаз
-function getWizardEyesColor () {
-  return  getRandomArrayElement(EYES_COLORS);
+// Возвращает цвет глаз
+function getWizardEyesColor() {
+  return getRandomArrayElement(EYES_COLORS);
 }
-//Возвращаем массив объектов волщебников
-function createWizards () {
+// Возвращаем массив объектов волщебников
+function createWizards() {
   var wizards = [];
   for (var i = 0; i < 4; i++) {
     wizards.push({
@@ -79,8 +79,8 @@ function createWizards () {
   }
   return wizards;
 }
-//Взвращает клонируемый из шаблона #similar-wizard-template блок с волшебником c заполнеными данными из массива
-function renderWizard (wizard) {
+// Возвращает клонируемый из шаблона #similar-wizard-template блок с волшебником c заполнеными данными из массива
+function renderWizard(wizard) {
   var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
   var wizardElement = similarWizardTemplate.cloneNode(true);
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
@@ -88,9 +88,9 @@ function renderWizard (wizard) {
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
   return wizardElement;
 }
-//fragment-oтстойник(Буфер). После того как собрался блок с волшебником. Этот блок отправляется в отстойник.
+// fragment-oтстойник(Буфер). После того как собрался блок с волшебником. Этот блок отправляется в отстойник.
 // Добавление идет в конец. И после того как всех волшебников собрали добавляем в блок setup-similar-list
-function showWizard () {
+function showWizard() {
   var similarListElement = document.querySelector('.setup-similar-list');
   var fragment = document.createDocumentFragment();
   var wizards = createWizards();
@@ -99,12 +99,12 @@ function showWizard () {
   }
   similarListElement.appendChild(fragment);
 }
-//Показываем блок setup
+// Показываем блок setup
 function showSetup() {
   document.querySelector('.setup').classList.remove('hidden');
 }
-//Показываем блок setup-similar
-function showSimilarList () {
+// Показываем блок setup-similar
+function showSimilarList() {
   document.querySelector('.setup-similar').classList.remove('hidden');
 }
 
